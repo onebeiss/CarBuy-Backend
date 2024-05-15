7c8959a4982df3cd6c905ab863e634b695ce7933
Víctor <victor.beis05@gmail.com>
2024-05-15 18:30:41 +0200

    Tablas creadas en models.py

--- CarBuy/carbuyrest22app/models.py
+++ CarBuy/carbuyrest22app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=120)
+    encrypted_password = models.CharField(max_length=120)
+    birthdate = models.DateField(auto_now_add=False)
+    token = models.CharField(unique=True, null=True, max_length=50)
+
+    def to_jsonAccount(self):
+        return {
+            "correo": self.email
+        }
+
+class Coche(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class CocheFavorito(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Coche, on_delete=models.CASCADE)
